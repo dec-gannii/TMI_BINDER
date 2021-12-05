747efdd471ca0bac9b8873f5e4a4b2671ff7363a
dec-gannii <rkdms1220@swu.ac.kr>
2021-12-05 23:24:59 +0900

    gaeun

---
+++ BINDER/BINDER/ViewController/ScheduleCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ScheduleCellTableViewCell.swift
+//  BINDER
+//
+//  Created by 김가은 on 2021/12/04.
+//
+
+import UIKit
+
+class ScheduleCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var scheduleDate: UILabel!
+    @IBOutlet weak var scheduleTitle: UILabel!
+    @IBOutlet weak var scheduleMemo: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
