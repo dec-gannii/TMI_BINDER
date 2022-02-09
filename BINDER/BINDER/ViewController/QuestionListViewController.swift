3375252f248d140a60ffb1f3ed402093f004fd58
YurimHa <78330615+YurimHa@users.noreply.github.com>
2022-02-09 14:39:45 +0900

    질문방 첫번째 화면 구현

---
+++ BINDER/BINDER/ViewController/QuestionListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionListViewController.swift
+//  BINDER
+//
+//  Created by 하유림 on 2022/02/09.
+//
+
+import UIKit
+import Kingfisher
+import Firebase
+
+class QuestionListViewController : BaseVC {
+    
+    
+}
---
+++ BINDER/BINDER/ViewController/models/QuestionListItem.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionListItem.swift
+//  BINDER
+//
+//  Created by 하유림 on 2022/02/09.
+//
+
+import Foundation
+
+struct QuestionListItme : Decodable {
+    
+    
+}
