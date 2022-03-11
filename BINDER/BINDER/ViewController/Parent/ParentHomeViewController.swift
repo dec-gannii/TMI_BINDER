f210e745948dbe6ac28c3a3f3199fbab9a0bcf82
dec-gannii <rkdms1220@swu.ac.kr>
2022-03-11 16:19:59 +0900

    db connected

--- BINDER/BINDER/ViewController/Parent/ParentHomeViewController.swift
+++ BINDER/BINDER/ViewController/Parent/ParentHomeViewController.swift
@@ -12,8 +12,27 @@ import Firebase
 class ParentHomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        getUserInfo()
     }
     
     @IBOutlet weak var parentNameLabel: UILabel!
     
+    func getUserInfo() {
+        let db = Firestore.firestore()
+        db.collection("parent").whereField("uid", isEqualTo: Auth.auth().currentUser!.uid).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(">>>>> document 에러 : \(err)")
+            } else {
+                if let err = err {
+                    print("Error getting documents(inMyClassView): \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        let name = document.data()["name"] as? String ?? ""
+                        self.parentNameLabel.text = name + " 학부모님"
+                    }
+                }
+            }
+        }
+    }
 }
--- BINDER/BINDER/ViewController/Parent/ParentMyPageViewController.swift
+++ BINDER/BINDER/ViewController/Parent/ParentMyPageViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Firebase
+import Kingfisher
 
 class ParentMyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        getUserInfo()
     }
     
     @IBOutlet weak var profileImageView: UIImageView!
@@ -20,10 +23,65 @@ class ParentMyPageViewController: UIViewController {
     
     @IBAction func DeleteChildInfoBtnClicked(_ sender: Any) {
     }
+    
     @IBAction func LogoutBtnClicked(_ sender: Any) {
-    }
-    @IBAction func GoSettingPageBtnClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Sign out error")
+        }
+        
+        if Auth.auth().currentUser != nil {
+            // Show logout page
+            let signinVC = self.storyboard?.instantiateViewController(withIdentifier: "SignInViewController")
+            signinVC?.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+            signinVC?.modalTransitionStyle = .crossDissolve //전환 애니메이션 설정
+            self.present(signinVC!, animated: true, completion: nil)
+        } else {
+            // Show login page
+            guard let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LogInViewController") as? LogInViewController else {
+                //아니면 종료
+                return
+            }
+            loginVC.modalTransitionStyle = .crossDissolve
+            loginVC.modalPresentationStyle = .fullScreen
+            loginVC.isLogouted = true
+            
+            self.present(loginVC, animated: true, completion: nil)
+        }
     }
     
+    @IBAction func GoSettingPageBtnClicked(_ sender: Any) {
+        guard let settingVC = self.storyboard?.instantiateViewController(withIdentifier: "SettingViewController") as? SettingViewController else {
+            //아니면 종료
+            return
+        }
+        settingVC.modalTransitionStyle = .crossDissolve
+        settingVC.modalPresentationStyle = .fullScreen
+        
+        self.present(settingVC, animated: true, completion: nil)
+    }
+    
+    func getUserInfo() {
+        let db = Firestore.firestore()
+        db.collection("parent").whereField("uid", isEqualTo: Auth.auth().currentUser!.uid).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(">>>>> document 에러 : \(err)")
+            } else {
+                if let err = err {
+                    print("Error getting documents(inMyClassView): \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        let profile = document.data()["profile"] as? String ?? "https://ifh.cc/g/Lt9Ip8.png"
+                        let name = document.data()["name"] as? String ?? ""
+                        self.nameLabel.text = name
+                        let url = URL(string: profile)!
+                        self.profileImageView.kf.setImage(with: url)
+                    }
+                }
+            }
+        }
+    }
     
 }
