bc01964f5a6f78c983f42660f83ae35a4289125c
YurimHa <78330615+YurimHa@users.noreply.github.com>
2022-03-12 09:25:02 +0900

    수업수정하기(db연결 오류)

--- BINDER/BINDER/ViewController/MyClass/EditClassVC.swift
+++ BINDER/BINDER/ViewController/MyClass/EditClassVC.swift
@@ -19,6 +19,12 @@ class EditClassVC : UIViewController {
     @IBOutlet var daysBtn: [UIButton]!
     
     @IBOutlet weak var cancelBtn: UIButton!
+    @IBAction func cancelBtnAction(_ sender: Any) {
+        if let preVC = self.presentingViewController as? UIViewController {
+            preVC.dismiss(animated: true, completion: nil)
+        }
+    }
+    
     @IBOutlet weak var okBtn: UIButton!
     
     let db = Firestore.firestore()
@@ -31,56 +37,48 @@ class EditClassVC : UIViewController {
     var repeatYN = ""
     var days = ""
     
-    var teacherItem: TeacherItem!
+    // var teacherItem: TeacherItem!
     var studentItem: StudentItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("class").document(self.studentItem.name + "(" + self.studentItem.email + ") " + self.subjectTF.text!).getDocument { [self] (document, error) in
-//            if let document = document, document.exists {
-//                let data = document.data()
-//                
-//                let subject = data?["subject"] as? String ?? ""
-//                self.subjectTF.text = subject
-//                
-//                let payType = data?["payType"] as? String ?? ""
-//                if (payType == "C") {
-//                    self.payTypeBtn.setTitle("회차별", for: .normal)
-//                } else {
-//                    self.payTypeBtn.setTitle("시간별", for: .normal)
-//                }
-//                
-//                let payAmount = data?["payAmount"] as? String ?? ""
-//                self.payAmountTF.text = payAmount
-//                
-//                let repeatYN = data?["repeatYN"] as? Bool ?? true
-//                if (repeatYN == true) {
-//                    self.repeatYNToggle.setOn(true, animated: true)
-//                } else {
-//                    self.repeatYNToggle.setOn(false, animated: true)
-//                }
-//                
-////                let schedule = data?["schedule"] as? String ?? ""
-////                // 저장된 스케줄을 " " 단위로 갈라내어 배열로 저장함
-////                schedule.components(separatedBy: " ")
-////                switch schedule {
-////                    case "월": daysBtn.
-////                    case "화":
-////                    case "수":
-////                    case "목":
-////                    case "금":
-////                    case "토":
-////                    case "일":
-////                }
-//
-//                
-//                
-//                
-//            } else {
-//                print("Document does not exist")
-//            }
-//        }
+        db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("class").document(self.studentItem.name + "(" + self.studentItem.email + ") " + self.subjectTF.text!).getDocument { [self] (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                
+                let subject = data?["subject"] as? String ?? ""
+                self.subjectTF.text = subject
+                
+                let payType = data?["payType"] as? String ?? ""
+                if (payType == "C") {
+                    self.payTypeBtn.setTitle("회차별", for: .normal)
+                } else {
+                    self.payTypeBtn.setTitle("시간별", for: .normal)
+                }
+                
+                let payAmount = data?["payAmount"] as? String ?? ""
+                self.payAmountTF.text = payAmount
+                
+                let repeatYN = data?["repeatYN"] as? Bool ?? true
+                if (repeatYN == true) {
+                    self.repeatYNToggle.setOn(true, animated: true)
+                } else {
+                    self.repeatYNToggle.setOn(false, animated: true)
+                }
+                
+                let schedule = data?["schedule"] as? String ?? ""
+//                 저장된 스케줄을 " " 단위로 갈라내어 배열로 저장함
+                schedule.components(separatedBy: " ")
+                print(schedule[schedule.startIndex])
+
+                
+                
+                
+            } else {
+                print("Document does not exist")
+            }
+        }
         
         
         
