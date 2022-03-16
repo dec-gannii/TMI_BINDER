6d011f73c618dc48105257b2235c440cbbb283b4
YurimHa <78330615+YurimHa@users.noreply.github.com>
2022-03-16 15:22:12 +0900

    나의 수업 수정하기

--- BINDER/BINDER/ViewController/MyClass/EditClassVC.swift
+++ BINDER/BINDER/ViewController/MyClass/EditClassVC.swift
@@ -11,6 +11,8 @@ import Firebase
 class EditClassVC : UIViewController {
     
     // 연결
+    
+    @IBOutlet weak var box: UIView!
     @IBOutlet weak var subjectTF: UITextField!
     @IBOutlet weak var payTypeBtn: UIButton!
     @IBOutlet weak var payAmountTF: UITextField!
@@ -48,6 +50,59 @@ class EditClassVC : UIViewController {
     }
     @IBOutlet weak var okBtn: UIButton!
     
+    
+    @IBAction func mondayBtn(_ sender: Any) {
+        if daysBtn[0].isSelected {
+            let monday = "월 "
+            schedule + monday
+        }
+    }
+    @IBAction func tuesdayBtn(_ sender: Any) {
+        if daysBtn[1].isSelected {
+            let tuesday = "화 "
+            schedule + tuesday
+        }
+    }
+    
+    @IBAction func wednesdayBtn(_ sender: Any) {
+        if daysBtn[2].isSelected {
+            let wednesday = "수 "
+            schedule + wednesday
+            // return schedule
+        }
+    }
+    
+    @IBAction func thursdayBtn(_ sender: Any) {
+        if daysBtn[3].isSelected {
+            let thursday = "목 "
+            schedule + thursday
+        }
+    }
+    
+    @IBAction func fridayBtn(_ sender: Any) {
+        if daysBtn[4].isSelected {
+            let friday = "금 "
+            schedule + friday
+        }
+    }
+    
+    @IBAction func saturdayBtn(_ sender: Any) {
+        if daysBtn[5].isSelected {
+            let saturday = "토 "
+            schedule + saturday
+        }
+    }
+    @IBAction func sundayBtn(_ sender: Any) {
+        
+        if daysBtn[6].isSelected {
+            let sunday = "일 "
+            schedule + sunday
+        }
+        
+    }
+
+    
+    
     var payType: PayType = .countly {
         didSet {
             changeUI()
@@ -68,14 +123,18 @@ class EditClassVC : UIViewController {
         }
     }
     
+    
     @IBAction func okBtnAction(_ sender: Any) {
+        
+
         
         db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("class").document(self.userName + "(" + self.userEmail + ") " + self.userSubject).updateData([
             "subject": subjectTF.text ?? "None",
             "payType": self.payType == .timly ? "T" : "C",
             "payAmount": payAmountTF.text ?? "None",
             "payDate": payDateTF.text ?? "None",
-            "repeatYN": repeatYN ?? true
+            "repeatYN": repeatYN ?? true,
+            "schedule": schedule ?? "None"
         ]) { err in
             if let err = err {
                 print("Error adding document: \(err)")
@@ -105,16 +164,18 @@ class EditClassVC : UIViewController {
         return repeatYNToggle.isOn
     }
     var days : [String] = []
+    var schedule = ""
     
     // var teacherItem: TeacherItem!
     var studentItem: StudentItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        box.allRound()
         // self.userName + "(" + self.userEmail + ") " + self.userSubject - 1
         // 에러 뜨는 위치 path에 위에 주석처리한 것처럼 하면 작동 되긴 함
         // 대신 DetailClassViewController에서 정보 넘기는 코드랑 위에 userName, userEmail, userSubject 변수 주석 처리 해제해야 함
+        
         
         db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("class").document(self.userName + "(" + self.userEmail + ") " + self.userSubject).getDocument { [self] (document, error) in
             if let document = document, document.exists {
