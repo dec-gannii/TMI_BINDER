3ae7b4b0d2623f0a9d9880c042b96bdd9ad9ced0
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-02 21:15:07 +0900

    BackEnd_CheckPW

--- BINDER/BINDER/Share/CustomFirebaseAPI.swift
+++ BINDER/BINDER/Share/CustomFirebaseAPI.swift
@@ -9,6 +9,8 @@ import Foundation
 import Firebase
 import FirebaseFirestore
 
+public var sharedCurrentPW : String = ""
+
 public func ShowScheduleList(type : String, date : String, datestr: String, scheduleTitles : [String], scheduleMemos : [String], count : Int) {
     let db = Firestore.firestore()
     
@@ -75,6 +77,40 @@ public func SetScheduleTexts(type : String, date : String, datestr: String, sche
                 // 가져온 내용들을 순서대로 일정 셀의 텍스트로 설정
                 scheduleCell.scheduleTitle.text = varScheduleTitles[i]
                 scheduleCell.scheduleMemo.text = varScheduleMemos[i]
+            }
+        }
+    }
+}
+
+public func GetPW() {
+    let db = Firestore.firestore()
+    
+    db.collection("teacher").document(Auth.auth().currentUser!.uid).getDocument { (document, error) in
+        if let document = document, document.exists {
+            let data = document.data()
+            // 현재 비밀번호 변수에 DB에 저장된 비밀번호 가져와서 할당
+            sharedCurrentPW = data?["password"] as? String ?? ""
+            print ("\(sharedCurrentPW) : sharedCurrentPW")
+        } else {
+            // 먼저 설정한 선생님 정보의 uid의 경로가 없다면 학생 정보에서 재탐색
+            db.collection("student").document(Auth.auth().currentUser!.uid).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    // 현재 비밀번호 변수에 DB에 저장된 비밀번호 가져와서 할당
+                    sharedCurrentPW = data?["password"] as? String ?? ""
+                    print ("\(sharedCurrentPW) : sharedCurrentPW1")
+                } else {
+                    db.collection("parent").document(Auth.auth().currentUser!.uid).getDocument { (document, error) in
+                        if let document = document, document.exists {
+                            let data = document.data()
+                            // 현재 비밀번호 변수에 DB에 저장된 비밀번호 가져와서 할당
+                            sharedCurrentPW = data?["password"] as? String ?? ""
+                            print ("\(sharedCurrentPW) : sharedCurrentPW2")
+                        } else {
+                            print("Document does not exist")
+                        }
+                    }
+                }
             }
         }
     }
