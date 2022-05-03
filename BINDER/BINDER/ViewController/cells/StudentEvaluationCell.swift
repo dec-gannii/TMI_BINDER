c4624cf0fd977215fa2f96ddfe4d5930d93f74b8
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-03 18:44:54 +0900

    BackEnd_StudentEvaluationCellGetEvaluation

--- BINDER/BINDER/ViewController/cells/StudentEvaluationCell.swift
+++ BINDER/BINDER/ViewController/cells/StudentEvaluationCell.swift
@@ -10,7 +10,7 @@ import Firebase
 import FirebaseDatabase
 import FirebaseFirestore
 
-class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+public class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     let months = ["01월", "02월", "03월", "04월", "05월", "06월", "07월", "08월", "09월", "10월", "11월", "12월"]
     let nowDate = Date()
     
@@ -37,10 +37,10 @@ class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerView
         self.teacherName = teacherName
         self.teacherEmail = teacherEmail
         self.subject = subject
-        getEvaluation()
+        GetEvaluationForStudentCell(self: self)
     }
     
-    override func awakeFromNib() {
+    public override func awakeFromNib() {
         super.awakeFromNib()
         LoadingHUD.isLoaded = false
         
@@ -71,19 +71,19 @@ class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerView
         self.selectionStyle = .none
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return months.count
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return months[row]
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         self.selectedMonth = months[row]
     }
     
@@ -93,57 +93,6 @@ class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerView
         pickerView.dataSource = self
         monthPickerView.tintColor = .clear
         monthPickerView.inputView = pickerView
-    }
-    
-    func getEvaluation() {
-        let db = Firestore.firestore()
-        db.collection("parent").whereField("uid", isEqualTo: Auth.auth().currentUser?.uid).getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print(">>>>> document 에러 : \(err)")
-            } else {
-                /// nil이 아닌지 확인한다.
-                guard let snapshot = querySnapshot, !snapshot.documents.isEmpty else {
-                    return
-                }
-                for document in snapshot.documents {
-                    if (LoadingHUD.isLoaded == false) {
-                        LoadingHUD.show()
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            LoadingHUD.isLoaded = true
-                            LoadingHUD.hide()
-                        }
-                    }
-                    print(">>>>> document 정보 : \(document.documentID) => \(document.data())")
-                    /// nil값 처리
-                    let childPhoneNumber = document.data()["childPhoneNumber"] as? String ?? ""
-                    db.collection("student").whereField("phonenum", isEqualTo: childPhoneNumber).getDocuments() { (querySnapshot, err) in
-                        if let err = err {
-                            print(">>>>> document 에러 : \(err)")
-                        } else {
-                            for document in querySnapshot!.documents {
-                                print("\(document.documentID) => \(document.data())")
-                                let studentUid = document.data()["uid"] as? String ?? ""
-                                db.collection("student").document(studentUid).collection("class").document(self.teacherName + "(" + self.teacherEmail + ") " + self.subject).collection("Evaluation").whereField("month", isEqualTo: self.selectedMonth).getDocuments() { (querySnapshot, err) in
-                                    if let err = err {
-                                        print(">>>>> document 에러 : \(err)")
-                                    } else {
-                                        self.setTextView("등록된 평가가 없습니다.")
-                                        self.monthPickerView.text = "\(self.selectedMonth)"
-                                        for document in querySnapshot!.documents {
-                                            let evaluation = document.data()["evaluation"] as? String ??  "등록된 평가가 없습니다."
-                                            self.setTextView(evaluation)
-                                            self.monthPickerView.text = "\(self.selectedMonth)"
-                                            LoadingHUD.isLoaded = true
-                                        }
-                                        self.monthPickerView.resignFirstResponder()
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
     
     func dismissPickerView() {
@@ -164,7 +113,7 @@ class StudentEvaluationCell: UITableViewCell, UIPickerViewDelegate, UIPickerView
     }
     
     @objc func donePicker() {
-        getEvaluation()
+        GetEvaluationForStudentCell(self: self)
     }
     
     @objc func cancelPicker() {
