1a254cad0e2f7acf8f25500e137cd8123ffe72a0
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-03 19:47:11 +0900

    BackEnd_MyPage
    - Save profile image
    - Save Toggle Button State
    - Get Portfolio Open State
    - Get User Info For My Page

--- BINDER/BINDER/Share/CustomFirebaseAPI.swift
+++ BINDER/BINDER/Share/CustomFirebaseAPI.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
+import FirebaseStorage
 
 public func ShowScheduleList(type : String, date : String, datestr: String, scheduleTitles : [String], scheduleMemos : [String], count : Int) {
     let db = Firestore.firestore()
@@ -75,6 +76,138 @@ public func SetScheduleTexts(type : String, date : String, datestr: String, sche
                 // 가져온 내용들을 순서대로 일정 셀의 텍스트로 설정
                 scheduleCell.scheduleTitle.text = varScheduleTitles[i]
                 scheduleCell.scheduleMemo.text = varScheduleMemos[i]
+            }
+        }
+    }
+}
+
+public func GetUserInfoForMyPage(self : MyPageViewController) {
+    let db = Firestore.firestore()
+    db.collection("teacher").document(Auth.auth().currentUser!.uid).getDocument { (document, error) in
+        if let document = document, document.exists {
+            LoginRepository.shared.doLogin {
+                self.nameLabel.text = "\(LoginRepository.shared.teacherItem!.name) 선생님"
+                self.teacherEmail.text = LoginRepository.shared.teacherItem!.email
+                self.type = "teacher"
+                let url = URL(string: LoginRepository.shared.teacherItem!.profile)!
+                self.imageView.kf.setImage(with: url)
+                self.imageView.makeCircle()
+            } failure: { error in
+                self.showDefaultAlert(msg: "")
+            }
+        } else {
+            db.collection("student").document(Auth.auth().currentUser!.uid).getDocument { (document, error) in
+                if let document = document, document.exists {
+                    let data = document.data()
+                    let userName = data?["name"] as? String ?? ""
+                    self.nameLabel.text = "\(userName) 학생"
+                    let userEmail = data?["email"] as? String ?? ""
+                    self.teacherEmail.text = userEmail
+                    let profile =  data?["profile"] as? String ?? "https://ifh.cc/g/Lt9Ip8.png"
+                    let goal = data?["goal"] as? String ?? "목표를 작성하지 않았습니다."
+                    self.type = "student"
+                    let url = URL(string: profile)!
+                    self.imageView.kf.setImage(with: url)
+                    self.imageView.makeCircle()
+                    self.viewDecorating()
+                    self.openPortfolioSwitch.removeFromSuperview()
+                    self.portfoiolBtn.removeFromSuperview()
+                    self.pageViewTitleLabel.text = "목표"
+                    self.pageViewContentLabel.text = goal
+                    self.pageViewContentLabel.numberOfLines = 2
+                    
+                    self.pageViewContentLabel.rightAnchor.constraint(equalTo: self.pageView.rightAnchor
+                                , constant: -20).isActive = true
+                    self.pageViewTitleLabel.font = UIFont.boldSystemFont(ofSize: 17.0)
+                    self.pageViewTitleLabel.topAnchor.constraint(equalTo: self.pageView.topAnchor
+                                , constant: 20).isActive = true
+                    self.pageView.heightAnchor.constraint(equalToConstant: 120)
+                                .isActive = true
+                } else {
+                    print("Document does not exist")
+                }
+            }
+        }
+    }
+}
+
+public func GetPortfolioShow(self : MyPageViewController) {
+    let db = Firestore.firestore()
+    
+    db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("Portfolio").document("portfolio").getDocument { (document, error) in
+        if let document = document, document.exists {
+            let data = document.data()
+            let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+            
+            let isShowOK = data?["portfolioShow"] as? String ?? ""
+            if (isShowOK == "On") {
+                self.openPortfolioSwitch.setOn(true, animated: true)
+            } else {
+                self.openPortfolioSwitch.setOn(false, animated: true)
+            }
+            
+            print("Document data: \(dataDescription)")
+        } else {
+            print("Document does not exist")
+        }
+    }
+}
+
+public func PortfolioToggleButtonClicked(self : MyPageViewController) {
+    let db = Firestore.firestore()
+    
+    if (self.openPortfolioSwitch.isOn) {
+        db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("Portfolio").document("portfolio").updateData([
+            "portfolioShow": "On"
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            }
+        }
+    } else {
+        db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("Portfolio").document("portfolio").updateData([
+            "portfolioShow": "Off"
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            }
+        }
+    }
+}
+
+public func SaveImage(self : MyPageViewController) {
+    let db = Firestore.firestore()
+    let storage = Storage.storage()
+    var storageRef = storage.reference()
+    
+    let image = self.imageView.image!
+    if let data = image.pngData(){
+        let urlRef = storageRef.child("image/\(self.profile!).png")
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/png"
+        let uploadTask = urlRef.putData(data, metadata: metadata){ (metadata, error) in
+            guard let metadata = metadata else {
+                return
+            }
+            
+            urlRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    return
+                }
+                
+                self.db.collection(self.type).document(Auth.auth().currentUser!.uid).updateData([
+                    "profile":"\(downloadURL)",
+                ]) { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    }
+                }
+                if (self.type == "teacher") {
+                    LoginRepository.shared.teacherItem?.profile = "\(downloadURL)"
+                } else if (self.type == "student") {
+                    LoginRepository.shared.studentItem?.profile = "\(downloadURL)"
+                }
             }
         }
     }
