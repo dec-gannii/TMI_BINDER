115bbb3a51a0b78abb162b884ff2e912c42b3d3b
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-04 22:54:45 +0900

    BackEnd_LogInVCFunctions

--- BINDER/BINDER/Share/CustomFirebaseAPI.swift
+++ BINDER/BINDER/Share/CustomFirebaseAPI.swift
@@ -8,6 +8,9 @@
 import Foundation
 import Firebase
 import FirebaseFirestore
+import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
 
 public func ShowScheduleList(type : String, date : String, datestr: String, scheduleTitles : [String], scheduleMemos : [String], count : Int) {
     let db = Firestore.firestore()
@@ -75,6 +78,238 @@ public func SetScheduleTexts(type : String, date : String, datestr: String, sche
                 // 가져온 내용들을 순서대로 일정 셀의 텍스트로 설정
                 scheduleCell.scheduleTitle.text = varScheduleTitles[i]
                 scheduleCell.scheduleMemo.text = varScheduleMemos[i]
+            }
+        }
+    }
+}
+
+public func LogInAndShowHomeVC (email : String, password: String, self : LogInViewController) {
+    let db = Firestore.firestore()
+    // 별 오류 없으면 로그인 되어서 홈 뷰 컨트롤러 띄우기
+    db.collection("parent").whereField("email", isEqualTo: email).getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+        } else {
+            for document in querySnapshot!.documents {
+                print("\(document.documentID) => \(document.data())")
+                // 사용할 것들 가져와서 지역 변수로 저장
+                guard let tb = self.storyboard?.instantiateViewController(withIdentifier: "ParentTabBarController") as? TabBarController else { return }
+                tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                self.present(tb, animated: true, completion: nil)
+                return
+            }
+            
+            guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else {
+                //아니면 종료
+                return
+            }
+            
+            // 아이디와 비밀번호 정보 넘겨주기
+            homeVC.pw = password
+            homeVC.id = email
+            if (Auth.auth().currentUser?.isEmailVerified == true){
+                homeVC.verified = true
+            } else { homeVC.verified = false }
+            
+            guard let myClassVC = self.storyboard?.instantiateViewController(withIdentifier: "MyClassViewController") as? MyClassVC else {
+                //아니면 종료
+                return
+            }
+            
+            guard let questionVC = self.storyboard?.instantiateViewController(withIdentifier: "QuestionViewController") as? QuestionViewController else {
+                return
+            }
+            guard let myPageVC =
+                    self.storyboard?.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController else {
+                return
+            }
+            
+            // tab bar 설정
+            let tb = UITabBarController()
+            tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+            tb.setViewControllers([homeVC, myClassVC, questionVC, myPageVC], animated: true)
+            self.present(tb, animated: true, completion: nil)
+        }
+    }
+}
+
+public func GoogleLogIn(googleCredential : AuthCredential, self : LogInViewController) {
+    Auth.auth().signIn(with: googleCredential) {
+        (authResult, error) in if let error = error {
+            print("Firebase sign in error: \(error)")
+            return
+        } else {
+            guard let TypeSelectVC = self.storyboard?.instantiateViewController(withIdentifier: "TypeSelectViewController") as? TypeSelectViewController else {
+                //아니면 종료
+                return
+            }
+            
+            //화면전환
+            if ((Auth.auth().currentUser) != nil) {
+                // 홈 화면으로 바로 이동
+                guard let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else {
+                    //아니면 종료
+                    return
+                }
+                
+                if (Auth.auth().currentUser?.isEmailVerified == true){
+                    homeVC.verified = true
+                } else { homeVC.verified = false }
+                
+                //화면전환
+                guard let myClassVC = self.storyboard?.instantiateViewController(withIdentifier: "MyClassViewController") as? MyClassVC else {
+                    //아니면 종료
+                    return
+                }
+                
+                guard let questionVC = self.storyboard?.instantiateViewController(withIdentifier: "QuestionViewController") as? QuestionViewController else {
+                    return
+                }
+                guard let myPageVC =
+                        self.storyboard?.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController else {
+                    return
+                }
+                
+                // tab bar 설정
+                let tb = UITabBarController()
+                tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                tb.setViewControllers([homeVC, myClassVC, questionVC, myPageVC], animated: true)
+                self.present(tb, animated: true, completion: nil)
+                
+                self.isLogouted = false
+            } else {
+                TypeSelectVC.isGoogleSignIn = true
+                TypeSelectVC.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                TypeSelectVC.modalTransitionStyle = .crossDissolve //전환 애니메이션 설정
+                self.present(TypeSelectVC, animated: true)
+            }
+        }
+    }
+}
+
+public func AppleLogIn(credential : OAuthCredential, self : LogInViewController) {
+    let db = Firestore.firestore()
+    // Sign in with Firebase.
+    Auth.auth().signIn(with: credential) { [weak self] (authResult, error) in
+        if (error != nil) {
+            print("ERROR : \(error)")
+            return
+        } else {
+            Firestore.firestore().collection("teacher").whereField("email", isEqualTo: Auth.auth().currentUser?.email).getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        
+                        let email = document.data()["email"] as? String ?? ""
+                        let password = document.data()["password"] as? String ?? ""
+                        
+                        guard let homeVC = self?.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else {
+                            //아니면 종료
+                            return
+                        }
+                        
+                        // 아이디와 비밀번호 정보 넘겨주기
+                        homeVC.pw = password
+                        homeVC.id = email
+                        
+                        if (Auth.auth().currentUser?.isEmailVerified == true){
+                            homeVC.verified = true
+                        } else { homeVC.verified = false }
+                        
+                        guard let myClassVC = self?.storyboard?.instantiateViewController(withIdentifier: "MyClassViewController") as? MyClassVC else {
+                            //아니면 종료
+                            return
+                        }
+                        
+                        guard let questionVC = self?.storyboard?.instantiateViewController(withIdentifier: "QuestionViewController") as? QuestionViewController else {
+                            return
+                        }
+                        guard let myPageVC =
+                                self?.storyboard?.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController else {
+                            return
+                        }
+                        
+                        // tab bar 설정
+                        let tb = UITabBarController()
+                        tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                        tb.setViewControllers([homeVC, myClassVC, questionVC, myPageVC], animated: true)
+                        self!.present(tb, animated: true, completion: nil)
+                        
+                    }
+                    Firestore.firestore().collection("student").whereField("email", isEqualTo: Auth.auth().currentUser?.email).getDocuments() { (querySnapshot, err) in
+                        if let err = err {
+                            print("Error getting documents: \(err)")
+                        } else {
+                            for document in querySnapshot!.documents {
+                                print("\(document.documentID) => \(document.data())")
+                                
+                                let email = document.data()["email"] as? String ?? ""
+                                let password = document.data()["password"] as? String ?? ""
+                                
+                                guard let homeVC = self?.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else {
+                                    //아니면 종료
+                                    return
+                                }
+                                
+                                // 아이디와 비밀번호 정보 넘겨주기
+                                homeVC.pw = password
+                                homeVC.id = email
+                                
+                                if (Auth.auth().currentUser?.isEmailVerified == true){
+                                    homeVC.verified = true
+                                } else { homeVC.verified = false }
+                                
+                                guard let myClassVC = self?.storyboard?.instantiateViewController(withIdentifier: "MyClassViewController") as? MyClassVC else {
+                                    //아니면 종료
+                                    return
+                                }
+                                
+                                guard let questionVC = self?.storyboard?.instantiateViewController(withIdentifier: "QuestionViewController") as? QuestionViewController else {
+                                    return
+                                }
+                                guard let myPageVC =
+                                        self?.storyboard?.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController else {
+                                    return
+                                }
+                                
+                                // tab bar 설정
+                                let tb = UITabBarController()
+                                tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                                tb.setViewControllers([homeVC, myClassVC, questionVC, myPageVC], animated: true)
+                                self!.present(tb, animated: true, completion: nil)
+                            }
+                            Firestore.firestore().collection("parent").whereField("email", isEqualTo: Auth.auth().currentUser?.email).getDocuments() { (querySnapshot, err) in
+                                if let err = err {
+                                    print("Error getting documents: \(err)")
+                                } else {
+                                    for document in querySnapshot!.documents {
+                                        print("\(document.documentID) => \(document.data())")
+                                        
+                                        guard let tb = self?.storyboard?.instantiateViewController(withIdentifier: "ParentTabBarController") as? TabBarController else { return }
+                                        tb.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                                        self!.present(tb, animated: true, completion: nil)
+                                        
+                                        return
+                                    }
+                                    // type select 화면으로 이동
+                                    guard let typeSelectVC = self?.storyboard?.instantiateViewController(withIdentifier: "TypeSelectViewController") as? TypeSelectViewController else { return }
+                                    typeSelectVC.modalPresentationStyle = .fullScreen
+                                    typeSelectVC.modalTransitionStyle = .crossDissolve
+                                    typeSelectVC.name = Auth.auth().currentUser?.displayName ?? ""
+                                    typeSelectVC.email = Auth.auth().currentUser?.email ?? ""
+                                    typeSelectVC.isAppleLogIn = true
+                                    
+                                    self!.present(typeSelectVC, animated: true, completion: nil)
+                                    return
+                                }
+                            }
+                        }
+                        return
+                    }
+                }
+                return
             }
         }
     }
