db4724af7811267484912937c58f53d93f0a9fb7
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-04 12:40:34 +0900

    BackEnd_ShowMyClassList

--- BINDER/BINDER/Share/CustomFirebaseAPI.swift
+++ BINDER/BINDER/Share/CustomFirebaseAPI.swift
@@ -79,3 +79,216 @@ public func SetScheduleTexts(type : String, date : String, datestr: String, sche
         }
     }
 }
+
+public func GetUserInfoForClassList(self : MyClassVC) {
+        let db = Firestore.firestore()
+        db.collection("teacher").whereField("uid", isEqualTo: Auth.auth().currentUser!.uid).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(">>>>> document 에러 : \(err)")
+            } else {
+                if let err = err {
+                    print("Error getting documents(inMyClassView): \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        let type = document.data()["type"] as? String ?? ""
+                        self.type = type
+                        let profile = document.data()["profile"] as? String ?? "https://ifh.cc/g/Lt9Ip8.png"
+                        
+                        let url = URL(string: profile)!
+                        self.teacherImage.kf.setImage(with: url)
+                        self.setTeacherInfo()
+                        
+                        LoadingHUD.show()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            LoadingHUD.hide()
+                        }
+                    }
+                }
+            }
+        }
+        
+        db.collection("student").whereField("uid", isEqualTo: Auth.auth().currentUser!.uid).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(">>>>> document 에러 : \(err)")
+            } else {
+                if let err = err {
+                    print("Error getting documents(inMyClassView): \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                        let type = document.data()["type"] as? String ?? ""
+                        self.type = type
+                        
+                        let profile = document.data()["profile"] as? String ?? "https://ifh.cc/g/Lt9Ip8.png"
+                        let url = URL(string: profile)!
+                        self.teacherImage.kf.setImage(with: url)
+                        
+                        self.setStudentInfo()
+                        
+                        LoadingHUD.show()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            LoadingHUD.hide()
+                        }
+                    }
+                }
+            }
+        }
+}
+
+public func SetMyClasses(self : MyClassVC) {
+    let db = Firestore.firestore()
+    db.collection("teacher").document(Auth.auth().currentUser!.uid).collection("class").getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print(">>>>> document 에러 : \(err)")
+            self.showDefaultAlert(msg: "클래스를 찾는 중 에러가 발생했습니다.")
+        } else {
+            /// nil이 아닌지 확인한다.
+            guard let snapshot = querySnapshot, !snapshot.documents.isEmpty else {
+                db.collection("student").document(Auth.auth().currentUser!.uid).collection("class").getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print(">>>>> document 에러 : \(err)")
+                        self.showDefaultAlert(msg: "클래스를 찾는 중 에러가 발생했습니다.")
+                    } else {
+                        /// nil이 아닌지 확인한다.
+                        guard let snapshot = querySnapshot, !snapshot.documents.isEmpty else {
+                            
+                            return
+                        }
+                        
+                        /// 조회하기 위해 원래 있던 것 들 다 지움
+                        self.classItems.removeAll()
+                        
+                        for document in snapshot.documents {
+                            print(">>>>> document 정보 : \(document.documentID) => \(document.data())")
+                            
+                            /// document.data()를 통해서 값 받아옴, data는 dictionary
+                            let classDt = document.data()
+                            
+                            self.type = "student"
+                            /// nil값 처리
+                            let email = classDt["email"] as? String ?? ""
+                            let name = classDt["name"] as? String ?? ""
+                            let goal = classDt["goal"] as? String ?? ""
+                            let subject = classDt["subject"] as? String ?? ""
+                            let currentCnt = classDt["currentCnt"] as? Int ?? 0
+                            let totalCnt = classDt["totalCnt"] as? Int ?? 0
+                            let classColor = classDt["circleColor"] as? String ?? "026700"
+                            let recentDate = classDt["recentDate"] as? String ?? ""
+                            let payType = classDt["payType"] as? String ?? ""
+                            let payDate = classDt["payDate"] as? String ?? ""
+                            let payAmount = classDt["payAmount"] as? String ?? ""
+                            let schedule = classDt["schedule"] as? String ?? ""
+                            let repeatYN = classDt["repeatYN"] as? String ?? ""
+                            let index = classDt["index"] as? Int ?? 0
+                            
+                            self.studentEmail = email
+                            
+                            let item = ClassItem(email: email, name: name, goal: goal, subject: subject, recentDate: recentDate, currentCnt: currentCnt, totalCnt: totalCnt, circleColor: classColor, payType: payType, payDate: payDate, payAmount: payAmount, schedule: schedule, repeatYN: repeatYN, index: index)
+                            
+                            /// 모든 값을 더한다.
+                            self.classItems.append(item)
+                        }
+                        
+                        /// UITableView를 reload 하기
+                        self.studentTV.reloadData()
+                    }
+                }
+                return
+            }
+            /// 조회하기 위해 원래 있던 것 들 다 지움
+            self.classItems.removeAll()
+            
+            
+            for document in snapshot.documents {
+                print(">>>>> document 정보 : \(document.documentID) => \(document.data())")
+                
+                /// document.data()를 통해서 값 받아옴, data는 dictionary
+                let classDt = document.data()
+                
+                self.type = "teacher"
+                /// nil값 처리
+                let email = classDt["email"] as? String ?? ""
+                let name = classDt["name"] as? String ?? ""
+                let goal = classDt["goal"] as? String ?? ""
+                let subject = classDt["subject"] as? String ?? ""
+                let currentCnt = classDt["currentCnt"] as? Int ?? 0
+                let totalCnt = classDt["totalCnt"] as? Int ?? 0
+                let classColor = classDt["circleColor"] as? String ?? "026700"
+                let recentDate = classDt["recentDate"] as? String ?? ""
+                let payType = classDt["payType"] as? String ?? ""
+                let payDate = classDt["payDate"] as? String ?? ""
+                let payAmount = classDt["payAmount"] as? String ?? ""
+                let schedule = classDt["schedule"] as? String ?? ""
+                let repeatYN = classDt["repeatYN"] as? String ?? ""
+                let index = classDt["index"] as? Int ?? 0
+                
+                let item = ClassItem(email: email, name: name, goal: goal, subject: subject, recentDate: recentDate, currentCnt: currentCnt, totalCnt: totalCnt, circleColor: classColor, payType: payType, payDate: payDate, payAmount: payAmount, schedule: schedule, repeatYN: repeatYN, index: index)
+                
+                /// 모든 값을 더한다.
+                self.classItems.append(item)
+            }
+            
+            /// UITableView를 reload 하기
+            self.studentTV.reloadData()
+        }
+    }
+}
+
+public func MoveToDetailClassVC (self : MyClassVC, sender : UIButton) {
+    var index: Int!
+    var name: String!
+    var email: String!
+    var subject: String!
+    var type: String!
+    
+    let db = Firestore.firestore()
+    /// 입력한 이메일과 갖고있는 이메일이 같은지 확인
+    var docRef: CollectionReference
+    if (self.type == "teacher") {
+        docRef = db.collection("teacher")
+    } else {
+        docRef = db.collection("student")
+    }
+    
+    docRef.document(Auth.auth().currentUser!.uid).collection("class").whereField("index", isEqualTo: sender.tag)
+        .getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(">>>>> document 에러 : \(err)")
+            } else {
+                guard let snapshot = querySnapshot, !snapshot.documents.isEmpty else {
+                    return
+                }
+                
+                guard let weekendVC = self.storyboard?.instantiateViewController(withIdentifier: "DetailClassViewController") as? DetailClassViewController else { return }
+                
+                weekendVC.modalPresentationStyle = .fullScreen //전체화면으로 보이게 설정
+                weekendVC.modalTransitionStyle = .crossDissolve //전환 애니메이션 설정
+                /// first : 여러개가 와도 첫번째 것만 봄.
+                
+                let studentDt = snapshot.documents.first!.data()
+                
+                if (self.type == "teacher") {
+                    index = studentDt["index"] as? Int ?? 0
+                    name = studentDt["name"] as? String ?? ""
+                    subject = studentDt["subject"] as? String ?? ""
+                    type = "teacher"
+                } else if (self.type == "student") {
+                    let teacherDt = snapshot.documents.first!.data()
+                    index = teacherDt["index"] as? Int ?? 0
+                    name = teacherDt["name"] as? String ?? ""
+                    type = "student"
+                    subject = teacherDt["subject"] as? String ?? ""
+                }
+                email = studentDt["email"] as? String ?? ""
+                
+                weekendVC.userIndex = index
+                weekendVC.userEmail = email
+                weekendVC.userName = name
+                weekendVC.userType = type
+                weekendVC.userSubject = subject
+                
+                self.present(weekendVC, animated: true, completion: nil)
+            }
+        }
+}
