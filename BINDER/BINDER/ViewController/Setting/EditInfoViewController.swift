6049bb1fd620d4fbc5d16c71092c0aca9b39025d
dec-gannii <rkdms1220@swu.ac.kr>
2022-05-06 10:32:08 +0900

    BackEnd_Edit Info Error Fix
    비밀번호 변경 없으면 공백으로 수정되는 부분 수정

--- BINDER/BINDER/ViewController/Setting/EditInfoViewController.swift
+++ BINDER/BINDER/ViewController/Setting/EditInfoViewController.swift
@@ -26,6 +26,7 @@ class EditInfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.currentPW = sharedCurrentPW
     }
     
     override func viewWillAppear(_ animated: Bool) {
